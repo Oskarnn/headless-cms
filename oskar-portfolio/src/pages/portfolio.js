ab37b5145e060f061461a0ffb4f6df17fa6d8136
Oskarnn <nygren.a.oskar@gmail.com>
2024-01-09 19:13:46 +0100

    cmt

--- oskar-portfolio/src/pages/portfolio.js
+++ oskar-portfolio/src/pages/portfolio.js
@@ -1,17 +1,70 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 
-import Layout from "../components/layout"
+import Layout from "../components/layout";
 
 //sidans namn blir portfolio efter namnet på javascript-filen
-const SecondPage = () => (
-  <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  const data = useStaticQuery(graphql`
+  query MyQuery {
+    allContentfulOskarsPortfolioLongTextTextNode {
+      edges {
+        node {
+          longText
+        }
+      }
+    }
+  }
+  `);
+  // HÄR MÅSTE DET EVENTUELLT ÄNDRAS
+  return (
+    <Layout>
+      <ul>
+        {data.allContentfulOskarsPortfolioLongTextTextNode.edges.map((edge) => {
+          return (
+            <li key={edge.node.id}>
+              <h2>{edge.node.hejHej}</h2>
+              <p>{edge.node.longText}</p>
+              <GatsbyImage
+                alt={edge.node.title}
+                // img={edge.node.heroImage.gatsbyImage}
 
-export const Head = () => <title>Portfolio Page</title>
+                // HÄR MÅSTE DET EVENTUELLT ÄNDRAS
+              />
+            </li>
+          );
+        })}
+      </ul>
 
-export default SecondPage
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  );
+};
+
+export const Head = () => <title>Portfolio Page</title>;
+
+export default SecondPage;
+
+
+
+// import * as React from "react"
+// import { Link } from "gatsby"
+
+// import Layout from "../components/layout"
+
+// //sidans namn blir portfolio efter namnet på javascript-filen
+// const SecondPage = () => (
+//   <Layout>
+//     <h1>Hi from the second page</h1>
+//     <p>Welcome to page 2</p>
+//     <Link to="/">Go back to the homepage</Link>
+//   </Layout>
+// )
+
+// export const Head = () => <title>Portfolio Page</title>
+
+// export default SecondPage
