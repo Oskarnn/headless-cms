93ef874e96331af788433bf733c12b9493ae5b84
Oskarnn <112063723+Oskarnn@users.noreply.github.com>
2024-01-11 14:22:07 +0100

    upfate

--- oskar-portfolio/src/pages/portfolio.js
+++ oskar-portfolio/src/pages/portfolio.js
@@ -8,25 +8,29 @@ import Layout from "../components/layout";
 //sidans namn blir portfolio efter namnet på javascript-filen
 const SecondPage = () => {
   const data = useStaticQuery(graphql`
-  query MyQuery {
-    allContentfulOskarsPortfolio {
-      edges {
-        node {
-          hejHej
+    query MyQuery {
+      allContentfulPost(sort: { title: ASC }) {
+        edges {
+          node {
+            title
+            description {
+              raw
+            }
+          }
         }
       }
     }
-  }
   `);
+
   // HÄR MÅSTE DET EVENTUELLT ÄNDRAS
   return (
     <Layout>
       <ul>
-        {data.allContentful.edges.map((edge) => {
+        {data.allContentfulPost.edges.map((edge) => {
           return (
             <li key={edge.node.id}>
-              <h2>{edge.node.hejHej}</h2>
-              <p>{edge.node.longText}</p>
+              <h2>{edge.node.title}</h2>
+              <p>{edge.node.description.raw}</p>
               <GatsbyImage
                 alt={edge.node.title}
                 // img={edge.node.heroImage.gatsbyImage}
@@ -37,9 +41,6 @@ const SecondPage = () => {
           );
         })}
       </ul>
-
-      <h1>Hi from the second page</h1>
-      <p>Welcome to page 2</p>
       <Link to="/">Go back to the homepage</Link>
     </Layout>
   );
@@ -47,24 +48,19 @@ const SecondPage = () => {
 
 export const Head = () => <title>Portfolio Page</title>;
 
+// export const query = graphql`
+//   query {
+//     allContentfulPost(sort: { title: ASC }) {
+//       edges {
+//         node {
+//           title
+//           description {
+//             raw
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
+
 export default SecondPage;
-
-
-
-// import * as React from "react"
-// import { Link } from "gatsby"
-
-// import Layout from "../components/layout"
-
-// //sidans namn blir portfolio efter namnet på javascript-filen
-// const SecondPage = () => (
-//   <Layout>
-//     <h1>Hi from the second page</h1>
-//     <p>Welcome to page 2</p>
-//     <Link to="/">Go back to the homepage</Link>
-//   </Layout>
-// )
-
-// export const Head = () => <title>Portfolio Page</title>
-
-// export default SecondPage
